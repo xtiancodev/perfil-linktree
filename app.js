e643d382ae784ae32249c46db04c9fc53bb13b20
Christian Parra <xtian3400@gmail.com>
2025-03-05 16:05:54 -0500

    HTML y CSS RECTIFICADO

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+// Esperar a que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', () => {
+    // Seleccionar el contenedor de información de perfil
     const profileInfoContainer = document.getElementById('profile-info');
 
-    // Crear elementos de información del perfil
+    // Crear elemento para el nombre usando JavaScript
     const nameElement = document.createElement('h2');
-    nameElement.textContent = 'Christian Parra';
+    nameElement.textContent = 'Lowell Silvia';
     
+    // Crear elemento para la descripción usando JavaScript
     const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = 'Software Developer based in Peru';
+    descriptionElement.textContent = 'Brand Ambassador for Helix, based in SoCal.';
 
     // Verificar la existencia del contenedor antes de agregar elementos
     if (profileInfoContainer) {
+        // Agregar nombre y descripción al contenedor
         profileInfoContainer.appendChild(nameElement);
         profileInfoContainer.appendChild(descriptionElement);
     }
